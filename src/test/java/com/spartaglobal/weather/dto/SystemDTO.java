a18a0845812b940a015abbb236f035a80d02aac9
joshuamc360 <joshuamc360@gmail.com>
2021-09-16 16:16:37 +0100

    System DTO tools for testing

--- src/test/java/com/spartaglobal/weather/dto/SystemDTO.java
+++ src/test/java/com/spartaglobal/weather/dto/SystemDTO.java
@@ -1,40 +1,72 @@
 package com.spartaglobal.weather.dto;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "type",
+        "id",
+        "message",
+        "country",
+        "sunrise",
+        "sunset"
+})
+@Generated("jsonschema2pojo")
+
 public class SystemDTO implements ISystemDTO{
 
+    @JsonProperty("type")
     private Integer type;
+    @JsonProperty("id")
     private Integer id;
+    @JsonProperty("message")
     private Double message;
+    @JsonProperty("country")
     private String country;
+    @JsonProperty("sunrise")
     private Long sunrise;
+    @JsonProperty("sunset")
     private Long sunset;
 
-    @Override
+    @JsonProperty("type")
     public Integer getType() {
         return type;
     }
 
-    @Override
+    @JsonProperty("id")
     public Integer getId() {
         return id;
     }
 
-    @Override
+    @JsonProperty("message")
     public Double getMessage() {
         return message;
     }
 
-    @Override
+    @JsonProperty("message")
+    public void setMessage(Double message) {
+        this.message = message;
+    }
+
+    @JsonProperty("country")
     public String getCountry() {
         return country;
     }
 
-    @Override
+    @JsonProperty("sunrise")
     public Long getSunrise() {
         return sunrise;
     }
 
-    @Override
+    @JsonProperty("sunset")
     public Long getSunset() {
         return sunset;
     }
