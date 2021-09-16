b9e8feb70cb37f5d1d31c59c363f2858859d8c17
JoeAH2000 <joehilton00@gmail.com>
2021-09-16 14:59:25 +0100

    Incomplete ConnectionManager, contains getURL methods

--- src/test/java/com/spartaglobal/weather/ConnectionManager.java
+++ src/test/java/com/spartaglobal/weather/ConnectionManager.java
@@ -1,4 +1,26 @@
 package com.spartaglobal.weather;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class ConnectionManager {
+    private static final String baseURL = "https://api.openweathermap.org/data/2.5/";
+
+    public static String getURL() {
+        return baseURL;
+    }
+
+    public static String getURL(String cityName, String apiKey) {
+        return (baseURL + "weather?q=" + cityName + "&appid=" + apiKey);
+    }
+
+    public static String getURL(int cityId, String apiKey) {
+        return (baseURL + "weather?id=" + cityId + "&appid=" + apiKey);
+    }
+
+//    public static int getStatusCode() {
+//        HttpClient httpClient = HttpClient.newHttpClient();
+//        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(baseURL))
+//    }
 }
