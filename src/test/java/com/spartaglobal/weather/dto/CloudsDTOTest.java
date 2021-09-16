8ee6baddef0cd3f4e1ec39a34ff520cc2c887b00
ShaisB <88349062+ShaisB@users.noreply.github.com>
2021-09-16 16:52:40 +0100

    Fixed weird git thing

--- src/test/java/com/spartaglobal/weather/dto/CloudsDTOTest.java
+++ src/test/java/com/spartaglobal/weather/dto/CloudsDTOTest.java
@@ -1,11 +1,9 @@
 package com.spartaglobal.weather.dto;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
 
 public class CloudsDTOTest {
 
@@ -16,12 +14,42 @@ public class CloudsDTOTest {
         cloudsDTO = new CloudsDTO();
     }
 
-    @Test
+
+    @Nested
     @DisplayName("Testing the percentage boundaries")
-    @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 100, 101})
-    void testPercentageBoundaries(int percentages){
-        Assertions.assertTrue(cloudsDTO.isPercentWithinBoundaries(percentages));
+    class PercentageBoundaries {
+
+        @ParameterizedTest
+        @DisplayName("Testing the out of bounds percentage boundaries")
+        @ValueSource(ints = {-1, 101})
+        void testPercentageOutOfBounds(int percentages) {
+            Assertions.assertFalse(cloudsDTO.isPercentWithinBoundaries(percentages));
+        }
+
+        @ParameterizedTest
+        @DisplayName("Testing the in bounds percentage boundaries")
+        @ValueSource(ints = {0, 100})
+        void testPercentageInBounds(int percentages) {
+            Assertions.assertTrue(cloudsDTO.isPercentWithinBoundaries(percentages));
+        }
+    }
+
+    @Nested
+    @DisplayName("Testing data types")
+    class TestDataTypes{
+
+        @Test
+        @DisplayName("Testing the boundaries return data type")
+        void testBoolean(){
+            Assertions.assertEquals(Boolean.class, cloudsDTO.isPercentWithinBoundaries(0).getClass());
+        }
+
+        @Test
+        @DisplayName("Testing the percentage return data type")
+        void testInteger(){
+            Assertions.assertEquals(Integer.class, cloudsDTO.getAll().getClass());
+        }
+
     }
 
 }
