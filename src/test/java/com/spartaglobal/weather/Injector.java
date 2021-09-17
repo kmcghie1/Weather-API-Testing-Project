50670b8308f1b0eb14e27bc9d3f8a2d6cb4f5b9e
CPlatts <cplatts@spartaglobal.com>
2021-09-17 10:24:29 +0100

    Added an injector into ProjectDTO for api path 'weather'

--- src/test/java/com/spartaglobal/weather/Injector.java
+++ src/test/java/com/spartaglobal/weather/Injector.java
@@ -1,4 +1,26 @@
 package com.spartaglobal.weather;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spartaglobal.weather.dto.ProjectDTO;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Injector {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static ProjectDTO injectDTO(String path) {
+        ProjectDTO dto = new ProjectDTO();
+        if(path.contains("weather")) {
+            try {
+                dto = objectMapper.readValue(new File(path), ProjectDTO.class);
+                return dto;
+            }catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
 }
